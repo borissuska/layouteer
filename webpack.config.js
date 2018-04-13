9fe88c417c32542eeafa6951da5608d6df9a4000
Boris Šuška <boris.suska@ysoft.com>
2018-04-13 16:39:35 +0200

    Updated config to make Remote debugging using Google Chrome via Visual Studio Code working

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,34 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
     mode: 'development',
-    entry: __dirname + '/src/index.js',
+    devtool: 'source-map',
+    entry: [
+        path.join(__dirname, 'src', 'index.js'),
+    ],
     output: {
-        path: __dirname + '/dist',
+        path: path.join(__dirname, '/dist'),
         publicPath: '/dist/',
         filename: 'layouteer.min.js'
     },
     module: {
         rules: [{
             test: /\.js$/,
+            include: path.join(__dirname, 'src'),
             exclude: /node_modules/,
-            use: 'babel-loader'
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['es2015']
+                }
+            }
+        }, {
+            test: /\.less$/,
+            use: "style-loader!css-loader!autoprefixer-loader!less-loader"
         }]
+    },
+    devServer: {
+        contentBase: path.join(__dirname)
     }
 };
